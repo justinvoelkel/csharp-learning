8a50f2ed4d2e151fd52c6d0d42ff80b2e0d542b3
Justin Voelkel <justin@budgetdumpster.com>
2017-06-21 14:53:32 -0400

    looping and abstracting response logic to try a method call

--- FitnessFrog/FitnessFrog/Program.cs
+++ FitnessFrog/FitnessFrog/Program.cs
@@ -7,13 +7,49 @@ namespace Treehouse.FitnessFrog
 		static void Main()
 		{
 			int total = 0;
+			bool cont = true;
 
-			Console.Write("Enter how many minutes you excercised: ");
+			while(cont)
+			{
+				Console.Write("Enter how many minutes you excercised or q to quit: ");
+				string input = Console.ReadLine();
 
-			string input = Console.ReadLine();
-			total += int.Parse(input);
+				if(input == "q"){
+					cont = false;
+					break;
+				} else {
+					try{
+						total += int.Parse(input);
+						string response = Program.responseLogic(total);
+						Console.WriteLine(response);
+						continue;
+					} catch(FormatException) {
+						Console.WriteLine("Sorry, that is not a valid input");
+						break;
+					}
+				}//end else
+			}//end while
+		}//end Main
 
-			Console.WriteLine(total);
-		}
-	}
+		public static string responseLogic(int hours)
+		{
+			string response = "Sorry, that input is invalid";
+
+			if(hours < 0) {
+				return response;
+			} else if(hours <= 10){
+				response = "Better than nothing I guess...";
+			} else if(hours > 10 && hours <= 30) {
+				response = "Not bad, getting better...";
+			} else if(hours > 30 && hours <= 60) {
+				response = "Damn, Daniel...";
+			} else if (hours > 60) {
+				response = "Showoff...";
+			}
+
+			return response;
+
+		}//end logic method
+
+	}//end Program
 }
